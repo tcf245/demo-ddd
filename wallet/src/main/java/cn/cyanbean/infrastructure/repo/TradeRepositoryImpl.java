890d54244a75a3851c8d6a20d4bab82595e1ce2e
chaofan.tan <chaofan.tan@colorv.com>
2022-03-29 10:25:43 +0800

    add jpa repo impl

--- wallet/src/main/java/cn/cyanbean/infrastructure/repo/TradeRepositoryImpl.java
+++ wallet/src/main/java/cn/cyanbean/infrastructure/repo/TradeRepositoryImpl.java
@@ -3,6 +3,7 @@ package cn.cyanbean.infrastructure.repo;
 import cn.cyanbean.domain.event.TradeEvent;
 import cn.cyanbean.domain.model.TradeRecord;
 import cn.cyanbean.domain.repository.TradeRepository;
+import cn.cyanbean.infrastructure.repo.jpa.JpaTradeRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,19 +12,25 @@ import java.util.Optional;
 @Repository
 public class TradeRepositoryImpl implements TradeRepository {
 
+    private JpaTradeRepository jpaTradeRepository;
+
+    public TradeRepositoryImpl(JpaTradeRepository jpaTradeRepository) {
+        this.jpaTradeRepository = jpaTradeRepository;
+    }
+
     @Override
     public TradeRecord save(TradeRecord tradeRecord) {
-        return null;
+        return jpaTradeRepository.save(tradeRecord);
     }
 
     @Override
     public TradeRecord findByTradeNumber(String tradeNumber) {
-        return null;
+        return jpaTradeRepository.findByTradeNumber(tradeNumber);
     }
 
     @Override
     public Optional<List<TradeRecord>> findAll() {
-        return Optional.empty();
+        return Optional.of(jpaTradeRepository.findAll());
     }
 
     @Override
--- wallet/src/main/java/cn/cyanbean/infrastructure/repo/WalletRepositoryImpl.java
+++ wallet/src/main/java/cn/cyanbean/infrastructure/repo/WalletRepositoryImpl.java
@@ -2,33 +2,36 @@ package cn.cyanbean.infrastructure.repo;
 
 import cn.cyanbean.domain.event.TradeEvent;
 import cn.cyanbean.domain.model.TradeRecord;
+import cn.cyanbean.domain.model.Wallet;
 import cn.cyanbean.domain.repository.TradeRepository;
+import cn.cyanbean.domain.repository.WalletRepository;
+import cn.cyanbean.infrastructure.repo.jpa.JpaWalletRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class WalletRepositoryImpl implements TradeRepository {
+public class WalletRepositoryImpl implements WalletRepository {
 
+    private JpaWalletRepository jpaWalletRepository;
 
-    @Override
-    public TradeRecord save(TradeRecord tradeRecord) {
-        return null;
+    public WalletRepositoryImpl(JpaWalletRepository jpaWalletRepository) {
+        this.jpaWalletRepository = jpaWalletRepository;
     }
 
     @Override
-    public TradeRecord findByTradeNumber(String tradeNumber) {
-        return null;
+    public Wallet save(Wallet wallet) {
+        return jpaWalletRepository.save(wallet);
     }
 
     @Override
-    public Optional<List<TradeRecord>> findAll() {
-        return Optional.empty();
+    public Wallet findById(String walletId) {
+        return jpaWalletRepository.findById(walletId).orElse(null);
     }
 
     @Override
-    public void sendMQEvent(TradeEvent tradeEvent) {
-
+    public Optional<List<Wallet>> findAll() {
+        return Optional.of(jpaWalletRepository.findAll());
     }
 }
